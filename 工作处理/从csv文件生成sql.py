d6e0fe627ea5637a2eefb584556198885ec7e5e8
MixJade <1114185977@qq.com>
2024-03-08 09:01:13 +0800

    优化样例sql

--- 工作处理/从csv文件生成sql.py
+++ 工作处理/从csv文件生成sql.py
@@ -12,7 +12,7 @@ df = df[['HOME_ID', 'DOG_TP', 'DOG_ID', '是否保留']]
 # 逐行打印SQL语句，同时需要保留的数据不删
 for index, row in df.iterrows():
     if row['是否保留'] != '保留':
-        print(f"DELETE Dog "
+        print(f"DELETE HomeAndDog "
               f"WHERE homeID='{row['HOME_ID']}' "
               f"AND dogID ='{row['DOG_ID']}' "
               f"AND dogTP='{row['DOG_TP']}';")
