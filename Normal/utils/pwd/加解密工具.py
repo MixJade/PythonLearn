44ad98df3e349b79b71f7efc4c13d8de36f3513e
MixJade <1114185977@qq.com>
2024-04-16 09:13:16 +0800

    加解密工具密钥补充

--- Normal/utils/pwd/加解密工具.py
+++ Normal/utils/pwd/加解密工具.py
@@ -12,6 +12,32 @@ from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 
 
+def pad_key_string(input_bytes: bytes) -> bytes:
+    """对输入的密钥进行处理，输出长度为16、24、32的数组"""
+    if len(input_bytes) < 24:
+        # 输出的数组长度小于24，则输出16位的密钥
+        return pad_iv_string(input_bytes)
+    elif 24 < len(input_bytes) < 32:
+        # 输出数组的长度大于24但小于32，则输出24位数组
+        return input_bytes[:24]
+    elif len(input_bytes) > 32:
+        # 输出数组的长度大于32，则输出32位数组
+        return input_bytes[:32]
+    else:
+        return input_bytes
+
+
+def pad_iv_string(input_bytes: bytes) -> bytes:
+    """对输入的偏移量进行处理，输出长度为16数组"""
+    pad_bytes = "TimeWaitForNoMan".encode()
+    if len(input_bytes) < 16:
+        return input_bytes + pad_bytes[len(input_bytes):16]
+    elif len(input_bytes) > 16:
+        return input_bytes[:16]
+    else:
+        return input_bytes
+
+
 def encrypt_text_aes(data: str, key1: bytes, iv1: bytes) -> str:
     """AES的CBC模式加密(只限文本)
 
@@ -20,6 +46,7 @@ def encrypt_text_aes(data: str, key1: bytes, iv1: bytes) -> str:
     :param iv1: 偏移量
     :return: 密文base64文本
     """
+    key1, iv1 = pad_key_string(key1), pad_iv_string(iv1)
     cipher = AES.new(key1, AES.MODE_CBC, iv1)
     ct_bytes = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
     base64_encrypted = b64encode(ct_bytes)
@@ -34,6 +61,7 @@ def decrypt_text_aes(ciphertext: str, key1: bytes, iv1: bytes) -> str:
     :param iv1: 偏移量
     :return: 明文
     """
+    key1, iv1 = pad_key_string(key1), pad_iv_string(iv1)
     cipher = AES.new(key1, AES.MODE_CBC, iv1)
     # base64解码为二进制数据
     cipher_byte = base64.b64decode(ciphertext)
@@ -47,7 +75,7 @@ if __name__ == "__main__":
     iv = input("请输入偏移量:").encode()
 
     while True:
-        print("===加密扣1,解密扣2,重设密钥扣3,终止按0===")
+        print("\n===加密扣1,解密扣2,重设密钥扣3,终止按0===")
         choose = input("请输入你的选择：")
         # match-case 语法(等于switch)
         match choose:
