07245a7c5df3a8aef5c9b886ca3575db9889f752
MixJade <1114185977@qq.com>
2024-04-18 20:05:17 +0800

    从配置文件链接数据库

--- Normal/school/华清远见/入门案例/链接数据库.py
+++ Normal/school/华清远见/入门案例/链接数据库.py
@@ -1,28 +1,46 @@
+import configparser
+
 import pymysql
+
+
+def get_connection():
+    """从配置文件读取数据库链接
+    """
+    config = configparser.ConfigParser()
+    config.read('mysql.ini')
+    return pymysql.connect(
+        host=config['mysql']['host'],
+        user=config['mysql']['user'],
+        passwd=config['mysql']['password'],
+        database=config['mysql']['database'])
 
 
 def connect_mysql():
     """连接数据库，查询，插入"""
-    connection = pymysql.connect(host="localhost", user="root", passwd="MC@:(==ni2024", database="play")
+    connection = get_connection()
     # 下面是py与mysql的通行车辆(游标)
-    curcat = connection.cursor()
+    cur_cat = connection.cursor()
     sq1 = "select * from students"
-    curcat.execute(sq1)
-    data01 = curcat.fetchall()
+    cur_cat.execute(sq1)
+    data01 = cur_cat.fetchall()
     print(data01)
-    print(curcat.fetchall())
+    print(cur_cat.fetchall())
 
     # 第二个操作
     print("第二次操作:")
     data02 = [['没头脑', 60, 60, 1.57, 300], ['不高兴', 60, 70, 1.66, 200]]
+    # 并不会真的插进去，
+    # 因为connect的autocommit默认为False，需要调用commit才能改数据库
     sq2 = "INSERT INTO students(studentName,englishGrade,mathGrade,height,money) VALUES(%s,%s,%s,%s,%s);"
-    curcat.executemany(sq2, data02)
+    cur_cat.executemany(sq2, data02)
     # 查询数据
-    curcat.execute(sq1)
-    data03 = curcat.fetchall()
+    cur_cat.execute(sq1)
+    data03 = cur_cat.fetchall()
     print(data03)
+    # 提交更改(不提交等于没有改数据库)
+    # connection.commit()
     # 关闭游标和连接
-    curcat.close()
+    cur_cat.close()
     connection.close()
 
 
