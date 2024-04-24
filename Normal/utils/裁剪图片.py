80434b282331fd6eb000156f2ff75c867191939d
MixJade <1114185977@qq.com>
2024-04-24 10:18:52 +0800

    图片裁剪方案增加

--- Normal/utils/裁剪图片.py
+++ Normal/utils/裁剪图片.py
@@ -20,12 +20,25 @@ def cut_img(file_path: str, x1: int, y1: int, x2: int, y2: int) -> None:
 if __name__ == '__main__':
     x_o, y_o, x_t, y_t = 0, 465, 1080, 1995
     print(f"当前点:{x_o},{y_o} {x_t},{y_t}")
-    # x_o, y_o = input("输入第1个点的坐标,逗号分割,如:1,2:").split(',')
-    # print(f"{x_o},{y_o}")
-    # x_t, y_t = input("输入第2个点的坐标,逗号分割,如:1,2:").split(',')
-    # print(f"{x_t},{y_t}")
     while True:
-        random_string = input("请输入文件路径(输入0退出)：")
+        random_string = input("输入文件路径(0退出/1重设坐标)：")
         if random_string == '0':
             break
-        cut_img(random_string, x_o, y_o, x_t, y_t)
+        elif random_string == '1':
+            print("""=========================
+    方案1 [0,465 1080,1995]
+    方案2 [0,0 1440,810]
+    其它n 自定义坐标
+=========================""")
+            plan_str = input("请选择你的方案:")
+            if plan_str == '1':
+                x_o, y_o, x_t, y_t = 0, 465, 1080, 1995
+            elif plan_str == '2':
+                x_o, y_o, x_t, y_t = 0, 0, 1440, 810
+            else:
+                # 输入格式如: 1 2
+                x_o, y_o = input("输入第1个点的坐标,空格分割:").split(' ')
+                x_t, y_t = input("输入第2个点的坐标,空格分割:").split(' ')
+            print(f"当前点:{x_o},{y_o} {x_t},{y_t}")
+        else:
+            cut_img(random_string, x_o, y_o, x_t, y_t)
