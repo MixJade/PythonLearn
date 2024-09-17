cd049e3cde5d6dbc8f9806364334dca4154dab2c
MixJade <111669273+MixJade@users.noreply.github.com>
2024-09-17 12:21:52 +0800

    中英文混排对齐.py

--- Normal/study/中英文混排对齐.py
+++ Normal/study/中英文混排对齐.py
@@ -1,38 +1,30 @@
 # coding=utf-8
 # @Time    : 2024/9/17 11:37
 # @Software: PyCharm
-def is_chinese(ch: str) -> bool:
-    if '\u4e00' <= ch <= '\u9fff':
-        return True
-    return False
 
+def align_text(title_str: str, max_len: int) -> str:
+    """返回加入了占位符的文字
 
-def align_text(title_key: str, max_english: int) -> str:
-    """返回加入了占位符的文字"""
-    chinese_count = 0
-    english_count = 0
-    for j in str(title_key):
-        if is_chinese(j):
-            chinese_count = chinese_count + 1
-        else:
-            english_count = english_count + 1
-
-    temp = max_english - english_count
-    while temp > 0:
-        title_key = title_key + ' '
-        temp = temp - 1
-    title_key = title_key.ljust(7, chr(12288))
-    return title_key
+    :param title_str: 需要处理的中英文混合字符串
+    :param max_len: 格式化后的最大长度(按英文空格计数)
+    :return: 最终格式化后的字符串
+    """
+    for j in str(title_str):
+        if '\u4e00' <= j <= '\u9fff':
+            max_len = max_len - 1
+    # 中文占两个字符,英文占一个,所以最终长度应该为(总长度-中文数)
+    # 左对齐,并填充字符串
+    return title_str.ljust(max_len, ' ')
 
 
 if __name__ == '__main__':
     test_data = [
-        ["张三123", 23],
+        ["张三zs123", 23],
         ["张三12", 523],
         ["李四94", 3],
-        ["星星9527", 223],
-        ["马云996", 23],
+        ["星星9527.py", 223],
+        ["马云jkm996", 23],
     ]
-    print(f"{align_text('姓名', 10)}年龄")
+    print(f"{align_text('姓名', 20)}年龄")
     for i in test_data:
-        print(f"{align_text(i[0], 10)}{i[1]}")
+        print(f"{align_text(i[0], 20)}{i[1]}")
