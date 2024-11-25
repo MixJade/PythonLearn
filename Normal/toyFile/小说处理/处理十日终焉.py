9932d56fcb5f5e3238e42085abea3df16baf53ca
MixJade <111669273+MixJade@users.noreply.github.com>
2024-11-25 20:46:11 +0800

    Update 处理十日终焉.py

--- Normal/toyFile/小说处理/处理十日终焉.py
+++ Normal/toyFile/小说处理/处理十日终焉.py
@@ -9,19 +9,33 @@ import re
 """
 
 # 构建文件路径(桌面上的文件)
-file_path = os.path.join(os.path.expanduser("~"), "Desktop/十日终焉.txt")
-results = []
+input_file_path = os.path.join(os.path.expanduser("~"), "Desktop/十日终焉.txt")
+output_file_path = os.path.join(os.path.expanduser("~"), "Desktop/十日终焉.md")
+results = [r"""---
+title: 《十日终焉》
+language: zh-CN
+---
+"""]
+
+# 正式处理
 pattern1 = re.compile(r'第\d+章')
+pattern2 = re.compile(r'第[一二三四五六七八九十百]+卷：')
 # 读取文件
-with open(file_path, "r", encoding='utf-8') as f:
+with open(input_file_path, "r", encoding='utf-8') as f:
     for line in f:
+        # '第xx章'、'第xx卷' 前面加上 '# '
         if line.startswith('第'):
-            # 判断该章节标题是否已经被标号
-            if pattern1.match(line):
+            if pattern1.match(line) or pattern2.match(line):
                 line = '# ' + line
+        # 正文缩进替换为4个空格(在起点、番茄等于1个汉字长度)
+        elif line.startswith('    '):
+            line = line.replace("    ", "&nbsp;" * 4)
+        # 分割下划线前加入换行符
+        elif line.startswith('------------'):
+            line = "\n" + line
         results.append(line)
 
 # 将替换后的内容写回文件
-with open("output_file.mds", 'w', encoding='utf-8') as file:
+with open(output_file_path, 'w', encoding='utf-8') as file:
     for line in results:
         file.write(line)
