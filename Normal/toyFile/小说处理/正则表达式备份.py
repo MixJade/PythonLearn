3a3b2e0b508d2ffd2f1aaad575ee121c556009ef
MixJade <1114185977@qq.com>
2025-02-11 17:59:53 +0800

    Update 正则表达式备份.py

--- Normal/toyFile/小说处理/正则表达式备份.py
+++ Normal/toyFile/小说处理/正则表达式备份.py
@@ -6,7 +6,7 @@ import re
 # 正则表达式(匹配长度少于12字的行)
 pattern1 = re.compile(r"^.{0,12}$")
 # 正则表达式(匹配中文章节)
-pattern2 = re.compile(r"第[一二三四五六七八九十百]+章")
+pattern2 = re.compile(r"第[零一二两三四五六七八九十百]+章")
 # 正则表达式(匹配数字章节)
 pattern3 = re.compile(r"第\d+章")
 # 正则表达式(匹配连续空行)
