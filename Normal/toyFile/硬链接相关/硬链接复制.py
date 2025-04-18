9442f48031e216f49cbb65c8930ab9b052a6d5a7
MixJade <1114185977@qq.com>
2025-04-18 15:06:12 +0800

    文件复制

---
+++ Normal/toyFile/硬链接相关/硬链接复制.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# @Time    : 2025/4/18 14:57
+# @Software: PyCharm
+import os
+
+
+def create_hard_link(source_file: str, destination_file: str) -> None:
+    try:
+        # 创建硬链接
+        os.link(source_file, destination_file)
+        print(f"硬链接已成功创建，从 {source_file} 到 {destination_file}")
+    except FileExistsError:
+        print(f"错误: 目标文件 {destination_file} 已存在。")
+    except FileNotFoundError:
+        print(f"错误: 源文件 {source_file} 未找到。")
+    except PermissionError:
+        print("错误: 没有足够的权限创建硬链接。")
+    except Exception as e:
+        print(f"发生未知错误: {e}")
+
+
+# 示例用法
+source_file_path = r"../../../../TsLearn/docs/从npm到pnpm.md"
+destination_file_path = '../../outputFile/destination.txt'
+create_hard_link(source_file_path, destination_file_path)
