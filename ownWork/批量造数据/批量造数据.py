14f2cb3a33aa3bcc3fbf28738f69d5f17f0faff5
MixJade <1114185977@qq.com>
2025-05-21 16:08:01 +0800

    Update 批量造数据.py

--- ownWork/批量造数据/批量造数据.py
+++ ownWork/批量造数据/批量造数据.py
@@ -2,10 +2,28 @@
 # @Time    : 2024/3/7 17:43
 # @Software: PyCharm
 from datetime import datetime
+import random
+
+NUMBER_LENGTH = 12
+LOWER_BOUND = 10 ** (NUMBER_LENGTH - 1)  # 12位数的最小值
+UPPER_BOUND = 10 ** NUMBER_LENGTH - 1  # 12位数的最大值
+generated_numbers: list = []
+
+
+def generate_unique_number():
+    """生成随机数
+    :return: 随机数(可以带前缀)
+    """
+    while True:
+        number = random.randint(LOWER_BOUND, UPPER_BOUND)
+        if number not in generated_numbers:
+            generated_numbers.append(number)
+            return f"Ran{number}"
+
 
 # 逐行打印SQL语句
-for i in range(0, 1000):
+for i in range(1, 1001):
     # noinspection SqlNoDataSourceInspection,SqlDialectInspection,SqlResolve
-    print(f"""INSERT INTO HomeAndDog (relId, homeID, dogID, dogTP)
-VALUES ('{datetime.now().strftime('%Y%m%d%H%M%S')}0{i}', '121', '211', '2');
+    print(f"""INSERT INTO HomeAndDog (relId,relNo, homeID, dogID, dogTP)
+VALUES ('{datetime.now().strftime('%Y%m%d%H%M%S')}{i:04d}',{generate_unique_number()}, '121', '211', '2');
     """)
