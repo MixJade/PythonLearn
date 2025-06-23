c5ed1b2c761c25c0b608995c49d7dbe11c85d8a8
MixJade <1114185977@qq.com>
2025-06-23 09:31:57 +0800

    csv提取大类去重优化

--- ownWork/从csv提取字段去重/csv提取大类去重.py
+++ ownWork/从csv提取字段去重/csv提取大类去重.py
@@ -4,14 +4,14 @@
 import pandas as pd
 
 df = pd.read_csv('有重复描述的字段.csv')
-df = df[['cate', 'code', 'dict']]
+df = df[['cate', 'dict_code', 'dict']]
 
 # 开始去重(指定列)，保留第一次出现的重复行
-df_unique2 = df.drop_duplicates(subset=['cate', 'code', 'dict'])
+df_unique2 = df.drop_duplicates(subset=['cate', 'dict_code', 'dict'])
 # 输出switch代码
 print('\n\nswitch (distributeCode) {')
 for index, row in df_unique2.iterrows():
-    print(f'  case "{row["code"]}": // {row["cate"]} {row["dict"]}\n    return "特殊表单";')
+    print(f'  case "{row["dict_code"]}": // {row["cate"]} {row["dict"]}\n    return "benpayrule0025";')
 print('}')
 
 # 输出sql代码
@@ -20,15 +20,16 @@ begin_seq = 3
 for index, row in df_unique2.iterrows():
     begin_seq += 1
     # noinspection SqlResolve,SqlNoDataSourceInspection,SqlDialectInspection
-    print(f'''insert into DICT (key_nm, code, name, seq)
-values ('CODE', '{row["cate"]}', '{row["dict"]}', '{begin_seq}');''')
+    print(f'''-- {row["cate"]} {row["dict"]}
+insert into DICT (key_nm, code, name, seq)
+values ('CODE', '{row["dict_code"]}', '{row["dict"]}', '{begin_seq}');''')
 
 # 基于field列去重，同时聚合多列
 df_agg3 = df_unique2.groupby('cate').agg({
-    'code': lambda x: ','.join(sorted(set(x)))
+    'dict_code': lambda x: ','.join(sorted(set(x)))
 }).reset_index()
 # 输出类别代码
 print("\n\n")
 for index, row in df_agg3.iterrows():
     print(f'// {row["cate"]}')
-    print(f'Code = ", ,{row["code"]}";')
+    print(f'Code = ", ,{row["dict_code"]}";')
