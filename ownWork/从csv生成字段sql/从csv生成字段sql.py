a0f83b0955e4ceaddfadcfa6777ef88da21977bc
MixJade <1114185977@qq.com>
2025-08-11 10:42:52 +0800

    update 从csv生成字段sql.py

--- ownWork/从csv生成字段sql/从csv生成字段sql.py
+++ ownWork/从csv生成字段sql/从csv生成字段sql.py
@@ -3,7 +3,7 @@ from utils.outFieldSQL import SqlParam, out_db_sql
 from utils.convertCase import small_snake_to_camel
 
 df = pd.read_csv('字段及对应注释.csv')
-df = df[['NAME', 'COMMENT']]
+df = df[['NAME', 'COMMENT', 'LENGTH']]
 
 sql_param_list: list[SqlParam] = []
 
@@ -12,7 +12,7 @@ for index, row in df.iterrows():
         java_name=small_snake_to_camel(row["NAME"]),
         comment=row["COMMENT"],
         sql_field=row["NAME"].upper(),
-        type_len='VARCHAR2(3)'
+        type_len=row["LENGTH"]
     ))
 
 table_name = "STUDENT_INF"
