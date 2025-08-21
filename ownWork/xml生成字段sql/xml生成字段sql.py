7f7fddb80153b33a354d33fd34ffad66415421ff
MixJade <1114185977@qq.com>
2025-08-21 11:09:44 +0800

    update xml生成字段sql.py

---
+++ ownWork/xml生成字段sql/xml生成字段sql.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# @Time    : 2025/8/21 10:59
+# @Software: PyCharm
+from lxml import etree
+from utils.outFieldSQL import SqlParam, out_db_sql
+from utils.convertCase import small_snake_to_camel
+
+with open(r"字段及对应注释.xml", 'rb') as xml_file:
+    xml_tree = etree.parse(xml_file)
+root = xml_tree.getroot()
+
+# 遍历每个分组并填入
+sql_param_list: list[SqlParam] = []
+# 遍历所有表配置
+for table in root.xpath('//table'):
+    # 遍历当前表字段
+    for field in table.xpath('./field'):
+        sql_param_list.append(SqlParam(
+            java_name=small_snake_to_camel(field.get('code')),
+            comment=field.get('name'),
+            sql_field=field.get('code').upper(),
+            type_len=field.get('length')
+        ))
+    out_db_sql(sql_param_list, table.get('code'), table.get('name'))
