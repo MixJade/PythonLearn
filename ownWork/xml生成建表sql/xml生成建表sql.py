825ee56ad26d0b605515cd78ce0fcc5de3267ace
MixJade <1114185977@qq.com>
2025-09-08 10:36:04 +0800

    Update xml生成建表sql.py

--- ownWork/xml生成建表sql/xml生成建表sql.py
+++ ownWork/xml生成建表sql/xml生成建表sql.py
@@ -10,10 +10,9 @@ with open(r"字段及注释_小驼峰.xml", 'rb') as xml_file:
     xml_tree = etree.parse(xml_file)
 root = xml_tree.getroot()
 
-# 遍历每个分组并填入
-sql_param_list: list[SqlParam] = []
 # 遍历所有表配置
 for table in root.xpath('//table'):
+    sql_param_list: list[SqlParam] = []
     # 遍历当前表字段
     for field in table.xpath('./field'):
         sql_param_list.append(SqlParam(
@@ -22,6 +21,7 @@ for table in root.xpath('//table'):
             sql_field=camel_to_snake(field.get('code')),
             type_len=field.get('length')
         ))
+    print(f"\n\n{'='*36}开始生成【{table.get('name')}】{'='*36}\n")
     # 生成建表SQL
     out_create_table_sql(sql_param_list, table.get('code'), table.get('name'))
     # 解开以下语句就是追加字段SQL
