c4064dc1d512510606f7208cb539731f42518d62
MixJade <1114185977@qq.com>
2025-09-18 09:23:46 +0800

    update xml生成建表sql

--- ownWork/xml生成建表sql/xml生成建表sql.py
+++ ownWork/xml生成建表sql/xml生成建表sql.py
@@ -3,10 +3,10 @@
 # @Software: PyCharm
 from lxml import etree
 
-from utils.convertCase import camel_to_snake
+from utils.convertCase import camel_to_snake, snake_to_camel
 from utils.outFieldSQL import *
 
-with open(r"字段及注释_小驼峰.xml", 'rb') as xml_file:
+with open(r"字段及注释_建表.xml", 'rb') as xml_file:
     xml_tree = etree.parse(xml_file)
 root = xml_tree.getroot()
 
@@ -15,13 +15,24 @@ for table in root.xpath('//table'):
     sql_param_list: list[SqlParam] = []
     # 遍历当前表字段
     for field in table.xpath('./field'):
-        sql_param_list.append(SqlParam(
-            java_name=field.get('code'),
-            comment=field.get('name'),
-            sql_field=camel_to_snake(field.get('code')),
-            type_len=field.get('length')
-        ))
-    print(f"\n\n{'='*36}开始生成【{table.get('name')}】{'='*36}\n")
+        field_code = field.get('code')
+        if ('_' in field_code) or (field_code[0].isupper()):
+            # 有下划线或第一个字符是大写，说明是大蛇形
+            sql_param_list.append(SqlParam(
+                java_name=snake_to_camel(field_code),
+                comment=field.get('name'),
+                sql_field=field_code,
+                type_len=field.get('length')
+            ))
+        else:
+            # 否则默认是小驼峰
+            sql_param_list.append(SqlParam(
+                java_name=field.get('code'),
+                comment=field.get('name'),
+                sql_field=camel_to_snake(field.get('code')),
+                type_len=field.get('length')
+            ))
+    print(f"\n\n{'=' * 36}开始生成【{table.get('name')}】{'=' * 36}\n")
     # 生成建表SQL
     out_create_table_sql(sql_param_list, table.get('code'), table.get('name'))
     # 解开以下语句就是追加字段SQL
