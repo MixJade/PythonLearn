16f7b60ef174514b46506b91334547e4b913abe4
MixJade <1114185977@qq.com>
2025-10-31 16:48:27 +0800

    update 批量替换文件内容.py

--- ownWork/批量替换文件内容/批量替换文件内容.py
+++ ownWork/批量替换文件内容/批量替换文件内容.py
@@ -1,55 +1,59 @@
 # coding=utf-8
-# @Time    : 2025/7/3 10:02
+# @Time    : 2025/10/31 16:42
 # @Software: PyCharm
 import os
+from lxml import etree
+from typing import NamedTuple
+
+"""
+批量替换多个文件中的字符串
+"""
+
+# 读取并解析XML
+with open("变更内容配置.xml", 'rb') as xml_file:
+    xml_tree = etree.parse(xml_file)
+root = xml_tree.getroot()
 
 
-def replace_in_files(file_paths, search_string, replace_string):
-    """
-    批量替换多个文件中的字符串
+class ChangeParam(NamedTuple):
+    old_str: str  # 旧字符串
+    new_str: str  # 新字符串
 
-    参数:
-    file_paths (list): 要处理的文件路径列表
-    search_string (str): 要查找的字符串
-    replace_string (str): 替换字符串
-    """
-    for file_path in file_paths:
-        if not os.path.isfile(file_path):
-            print(f"错误: 文件 '{file_path}' 不存在")
-            continue
 
-        try:
-            # 读取文件内容
-            with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
+# 文件列表
+file_list: list[str] = []
+for file1 in root.xpath('//fileList'):
+    for index, parm in enumerate(file1.xpath('./file')):
+        file_list.append(parm.get('path'))
 
-            # 替换字符串
-            if search_string in content:
-                new_content = content.replace(search_string, replace_string)
+# 变更列表
+change_list: list[ChangeParam] = []
+for change1 in root.xpath('//changeList'):
+    for index, parm in enumerate(change1.xpath('./change')):
+        old_str = parm.get('old')
+        new_str = parm.get('new')
+        change_list.append(ChangeParam(old_str, new_str))
 
-                # 写回文件
-                with open(file_path, 'w', encoding='utf-8') as file:
-                    file.write(new_content)
-
-                print(f"已替换文件: {file_path}")
+# 开始替换
+for file_path in file_list:
+    print(f"\n开始替换文件: {file_path}")
+    if not os.path.isfile(file_path):
+        print(f"错误: 文件 '{file_path}' 不存在")
+        continue
+    try:
+        # 读取文件内容
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        # 替换字符串
+        for change in change_list:
+            if change.old_str in content:
+                content = content.replace(change.old_str, change.new_str)
+                print(" " * 4 + f"{change.old_str} --> {change.new_str}")
             else:
-                print(f"文件 '{file_path}' 中未找到目标字符串")
-        except Exception as e:
-            print(f"处理文件 '{file_path}' 时出错: {e}")
-
-
-if __name__ == "__main__":
-    files_path = []  # 要处理的文件路径列表
-    search_txt = '保留第一次出现的重复行'  # 要处理的文件路径列表"
-    replace_txt = '保留第1次出现的重复行'  # 替换字符串
-
-    file_dir = "../从csv提取字段去重"
-    files_name = ['csv分组输出.py', 'csv提取大类去重.py']
-    for i in range(0, len(files_name)):
-        files_path.append(os.path.join(file_dir, files_name[i]))
-
-    # 最后替换(如果要换回去的话，可调整此处参数位置)
-    replace_in_files(files_path,
-                     search_txt,
-                     replace_txt
-                     )
+                print(" " * 4 + f"No find: {change.old_str}")
+        # 写回文件
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+        print("替换完成")
+    except Exception as e:
+        print(f"处理文件 '{file_path}' 时出错: {e}")
